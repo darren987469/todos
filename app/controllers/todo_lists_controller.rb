c907362efb1ef42f934ae2a2593f7aca3a7e9c6c
darren.chang <darren987469@gmail.com>
2018-05-11 22:54:50 +0800

    Use websocket to do todos CRUD

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,9 +1,11 @@
 class TodoListsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @todo_lists = TodoList.all
   end
 
   def show
-    @todo_list = TodoList.includes(:todos).find(params[:id])
+    @todo_list = TodoList.find(params[:id])
   end
 end
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -4,35 +4,41 @@ class TodosController < ApplicationController
   def create
     @todo = Todo.new(todo_params.merge(todo_list_id: params[:todo_list_id]))
     if @todo.save
-      ActionCable.server.broadcast 'todo_list', todo: @todo
-      render json: @todo
+      success_response
     else
-      render json: { errors: @todo.errors.messages }, status: :bad_request
+      fail_response
     end
   end
 
   def update
     @todo = Todo.find_by(todo_list_id: params[:todo_list_id], id: params[:id])
     if @todo.update(todo_params)
-      ActionCable.server.broadcast 'todo_list', todo: @todo
-      render json: @todo
+      success_response
     else
-      render json: { errors: @todo.errors.messages }, status: :bad_request
+      fail_response
     end
   end
 
   def destroy
     @todo = Todo.find_by(todo_list_id: params[:todo_list_id], id: params[:id])
     if @todo.destroy
-      ActionCable.server.broadcast 'todo_list', todo: @todo
-      render json: @todo
+      success_response
     else
-      render json: { errors: @todo.errors.messages }, status: :bad_request
+      fail_response
     end
   end
 
   private
     def todo_params
       params.require(:todo).permit(:description, :complete)
+    end
+
+    def success_response
+      ActionCable.server.broadcast "todo_list_#{params[:todo_list_id]}", todo: @todo
+      render json: @todo
+    end
+
+    def fail_response
+      render json: { errors: @todo.errors.messages }, status: :bad_request
     end
 end
