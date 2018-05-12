1cd756874355976f588b66f7ccd6c6067e1aadea
darren.chang <darren987469@gmail.com>
2018-05-12 23:03:02 +0800

    Refine todo_lists controller

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -2,11 +2,17 @@ class TodoListsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @todo_lists = TodoList.all
+    @todo_list = current_user.todo_lists.first
+    @todo_list = TodoList.build(name: 'New Todo List', user: current_user) if @todo_list.blank?
+
+    redirect_to todo_list_path(@todo_list)
   end
 
   def show
-    @todo_list = TodoList.find(params[:id])
+    @todo_lists = current_user.todo_lists
+    @todo_list = @todo_lists.find { |todo_list| todo_list.id == params[:id].to_i }
+    raise ActiveRecord::RecordNotFound unless @todo_list.present?
+
     @todos = @todo_list.todos.order(id: :asc)
     @logs = EventLog.where(resourceable: @todos).order(id: :desc).limit(10)
   end
