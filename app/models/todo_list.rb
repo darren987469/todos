9246514dd9c212bfc17917522fda69bbd4e13047
darren.chang <darren987469@gmail.com>
2018-05-12 22:12:04 +0800

    Add todo_list_ships relations

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,4 +1,10 @@
 class TodoList < ApplicationRecord
   has_many :todos
   has_many :logs, as: :resourceable, class_name: 'EventLog'
+  has_many :todo_listships, dependent: :delete_all, class_name: '::TodoListships'
+  has_many :users, through: :todo_listships
+
+  def owner
+    users.merge(TodoListships.owner).first
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :todo_listships, class_name: '::TodoListships'
+  has_many :todo_lists, through: :todo_listships, class_name: '::TodoList'
 
   def full_name
     "#{first_name} #{last_name}"
