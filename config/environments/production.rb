97cac81632a147c286cba4bd0700a0f397c849dc
darren.chang <darren987469@gmail.com>
2018-05-15 21:17:31 +0800

    Add heroku production setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = 'wss://todos-actioncable.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [
+    'https://todos-actioncable.herokuapp.com',
+    'http://todos-actioncable.herokuapp.com'
+  ]
 end
