1f7325d5c541eaa6f751b04a514e5d17044eb424
darren987469 <darren987469@gmail.com>
2018-10-08 23:17:39 +0800

    Temp commit

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
 
 class Todo < ApplicationRecord
+  include Searchable
+
   belongs_to :todo_list
   has_many :logs, as: :resourceable, class_name: 'EventLog'
 
   scope :active, -> { where(archived_at: nil) }
   scope :archived, -> { where.not(archived_at: nil) }
+
+  settings index: { number_of_shards: 1 } do
+    mappings dynamic: 'false' do
+      indexes :description, analyzer: 'english', index_options: 'offsets'
+    end
+  end
 end
